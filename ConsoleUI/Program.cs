100f772d836e8806375dba5d935bf7828cde9bc1
esra.cetintas@outlook.com.tr <esra.cetintas@outlook.com.tr>
2021-03-30 12:34:14 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,62 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            CarAddTest(carManager);
+            CarGetAllTest(carManager);
+            Console.WriteLine("------------->");
+            CarDeleteTest(carManager);
+
+            Console.WriteLine("BrandId");
+
+            CarGetAllBrandId(carManager);
+
+            Console.WriteLine("------------->");
+
+            CarUpdateTest(carManager);
+
+            CarGetAllBrandId(carManager);
+
+        }
+
+        private static void CarGetAllBrandId(CarManager carManager)
+        {
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.BrandId);
+            }
+        }
+
+        private static void CarUpdateTest(CarManager carManager)
+        {
+            carManager.Update(new Car { Id = 6, BrandId = 30 });
+        }
+
+        private static void CarDeleteTest(CarManager carManager)
+        {
+            carManager.Delete(new Car { Id = 7, BrandId = 15, ColorId = 1, ModelYear = 2020, DailyPrice = 350, Description = "Kiralık Araç" });
+        }
+
+        private static void CarGetAllTest(CarManager carManager)
+        {
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id);
+            }
+        }
+
+        private static void CarAddTest(CarManager carManager)
+        {
+            carManager.Add(new Car { Id = 6, BrandId = 5, ColorId = 0, ModelYear = 2012, DailyPrice = 250, Description = "Kiralık Araç" });
+            carManager.Add(new Car { Id = 7, BrandId = 15, ColorId = 1, ModelYear = 2020, DailyPrice = 350, Description = "Kiralık Araç" });
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=5,ColorId=0,ModelYear=1998,DailyPrice=100,Description="kiralık"},
+                new Car{Id=2,BrandId=5,ColorId=0,ModelYear=2003,DailyPrice=150,Description="kiralık"},
+                new Car{Id=3,BrandId=10,ColorId=1,ModelYear=2012,DailyPrice=300,Description="kiralık"},
+                new Car{Id=4,BrandId=10,ColorId=3,ModelYear=2005,DailyPrice=200,Description="kiralık"},
+                new Car{Id=5,BrandId=15,ColorId=2,ModelYear=2009,DailyPrice=250,Description="kiralık"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
